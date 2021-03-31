32d1c906769850e96fe1e399751d197fe14400f8
jimin9576 <wlals9576@naver.com>
2021-03-31 09:34:04 +0900

    Button app with inner class

---
+++ app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.btnwiinnerclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
